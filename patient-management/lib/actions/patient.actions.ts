4aa5e5c3184c3c1e783c132c53755af62522f78d
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-11-21 20:40:03 +0300

    registration 1

--- patient-management/lib/actions/patient.actions.ts
+++ patient-management/lib/actions/patient.actions.ts
@@ -1,16 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-
-import {
-  users,
-} from "../appwrite.config";
+import { users } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
-// CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
   try {
-    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
     const newuser = await users.create(
       ID.unique(),
       user.email,
@@ -32,3 +27,13 @@ export const createUser = async (user: CreateUserParams) => {
     console.error("An error occurred while creating a new user:", error);
   }
 };
+
+export const getUser = async (userId: string) => {
+  try{
+    const user = await users.get(userId);
+
+    return parseStringify(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
