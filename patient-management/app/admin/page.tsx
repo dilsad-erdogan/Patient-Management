a450ebc9cb87e15be362c1335fc208b2b7636e65
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-12-11 22:35:41 +0300

    appointment modal, form

--- patient-management/app/admin/page.tsx
+++ patient-management/app/admin/page.tsx
@@ -1,33 +1,40 @@
+import { DataTable } from '@/components/table/DataTable'
 import StatCard from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { columns } from '@/components/table/columns'
 
-const Admin = () => {
-  return (
-    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
-        <header className='admin-header'>
-            <Link href="/" className='cursor-pointer'>
-                <Image src="/assets/icons/logo-full.svg" height={32} width={162} alt='Logo' className='h-8 w-fit'/>
-            </Link>
+const Admin = async () => {
+    const appointments = await getRecentAppointmentList();
 
-            <p className='text-16-semibold'>Admin Dashboard</p>
-        </header>
+    return (
+        <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+            <header className='admin-header'>
+                <Link href="/" className='cursor-pointer'>
+                    <Image src="/assets/icons/logo-full.svg" height={32} width={162} alt='Logo' className='h-8 w-fit'/>
+                </Link>
 
-        <main className='admin-main'>
-            <section className='w-full space-y-4'>
-                <h1 className='header'>Welcome 👋</h1>
-                <p className='text-dark-700'>Start the day with managing new appointments</p>
-            </section>
+                <p className='text-16-semibold'>Admin Dashboard</p>
+            </header>
 
-            <section className='admin-stat'>
-                <StatCard type="appointments" count={5} label="Scheduled appointments" icon="/assets/icons/appointments.svg" />
-                <StatCard type="pending" count={10} label="Pending appointments" icon="/assets/icons/pending.svg" />
-                <StatCard type="cancelled" count={2} label="Cancelled appointments" icon="/assets/icons/cancelled.svg" />
-            </section>
-        </main>
-    </div>
-  )
+            <main className='admin-main'>
+                <section className='w-full space-y-4'>
+                    <h1 className='header'>Welcome 👋</h1>
+                    <p className='text-dark-700'>Start the day with managing new appointments</p>
+                </section>
+
+                <section className='admin-stat'>
+                    <StatCard type="appointments" count={appointments.scheduledCount} label="Scheduled appointments" icon="/assets/icons/appointments.svg" />
+                    <StatCard type="pending" count={appointments.pendingCount} label="Pending appointments" icon="/assets/icons/pending.svg" />
+                    <StatCard type="cancelled" count={appointments.cancelledCount} label="Cancelled appointments" icon="/assets/icons/cancelled.svg" />
+                </section>
+
+                <DataTable columns={columns} data={appointments.documents} />
+            </main>
+        </div>
+    )
 }
 
 export default Admin
---
+++ patient-management/components/table/columns.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import StatusBadge from "../StatusBadge"
+import { formatDateTime } from "@/lib/utils"
+import { Doctors } from "@/constants"
+import Image from "next/image"
+import AppointmentModal from "../AppointmentModal"
+
+export type Payment = {
+  id: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+}
+
+export const columns: ColumnDef<Payment>[] = [
+    {
+        header: 'ID',
+        cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>
+    },
+    {
+        accessorKey: 'patient',
+        header: 'Patient',
+        cell: ({ row }) => <p className="text-14-medium">{row.original.patient.name}</p>
+    },
+    {
+        accessorKey: "status",
+        header: "Status",
+        cell: ({ row }) => (
+            <div className="min-w-[115px]">
+                <StatusBadge status={row.original.status} />
+            </div>
+        )
+    },
+    {
+        accessorKey: "status",
+        header: "Appointment",
+        cell: ({ row }) => (
+            <p className="text-14-regular min-w-[100px]">
+                {formatDateTime(row.original.schedule).dateTime}
+            </p>
+        )
+    },
+    {
+        accessorKey: "primaryPhysician",
+        header: "Doctor",
+        cell: ({ row }) => {
+          const appointment = row.original;
+    
+          const doctor = Doctors.find(
+            (doctor) => doctor.name === appointment.primaryPhysician
+          );
+    
+          return (
+            <div className="flex items-center gap-3">
+              <Image src={doctor?.image!} alt="doctor" width={100} height={100} className="size-8" />
+              <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+            </div>
+          );
+        },
+    },
+    {
+        id: "actions",
+        header: () => <div className="pl-4">Actions</div>,
+        cell: ({ row: { original: data } }) => {
+            return (
+                <div className="flex gap-1">
+                    <AppointmentModal type="schedule" patientId={data.patient.$id} userId={data.userId} appointment={data} />
+                    <AppointmentModal type="cancel" patientId={data.patient.$id} userId={data.userId} appointment={data} />
+                </div>
+            )
+        },
+    },
+]
